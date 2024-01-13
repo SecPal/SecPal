70a70cfccb0ae2720b202db478f6129768e39d72
Holger Schmermbeck <hosch@mailbox.org>
2024-01-13 19:45:21 +0100

    refactor

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -4,12 +4,6 @@
  * Copyright (c) 2024 Holger Schmermbeck. Licensed under the EUPL-1.2 or later.
  */
 
-/**
- * Class Login
- *
- * This class represents the login component for the Livewire authentication module.
- */
-
 namespace App\Livewire\Auth;
 
 use Illuminate\Support\Facades\Auth;
@@ -30,38 +24,52 @@ class Login extends Component
 
     public function mount(): void
     {
-        $this->checkRedirect();
+        $this->checkForRedirect();
     }
 
-    private function checkRedirect(): void
+    private function checkForRedirect(): void
     {
-        ray('check redirect');
         // check if we want to redirect after successful login
         if (url()->current() != url()->previous()) {
-            ray('set redirect route');
             $this->redirectUrl = str_replace(url('/'), '', url()->previous());
         }
     }
 
     public function login(): void
     {
-        $this->validate();
+        $this->validateUserCredentials();
+        $this->attemptLogin();
+    }
 
+    private function validateUserCredentials(): void
+    {
+        $this->validate();
+    }
+
+    private function attemptLogin(): void
+    {
         if (Auth::attempt(['username' => $this->username, 'password' => $this->password])) {
             session()->regenerate();
-
-            // redirect to previous url or to the dashboard
-            if ($this->redirectUrl) {
-                ray('redirect');
-                $this->redirect($this->redirectUrl, navigate: true);
-            } else {
-                ray('home route');
-                $this->redirectRoute('dashboard', navigate: true);
-            }
+            $this->handleSuccessfulLogin();
         } else {
-            $this->addError('username', __('auth.failed'));
-            $this->showErrorIndicator = true;
+            $this->handleFailedLogin();
         }
+    }
+
+    private function handleSuccessfulLogin(): void
+    {
+        // redirect to previous url or to the dashboard
+        if ($this->redirectUrl) {
+            $this->redirect($this->redirectUrl, navigate: true);
+        } else {
+            $this->redirectRoute('dashboard', navigate: true);
+        }
+    }
+
+    private function handleFailedLogin(): void
+    {
+        $this->addError('username', __('auth.failed'));
+        $this->showErrorIndicator = true;
     }
 
     public function render()
