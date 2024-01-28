3b564d955fdfff126e384e72bb48669893ad1a6a
Holger Schmermbeck <hosch@mailbox.org>
2024-01-28 07:31:08 +0100

    Hide user-menu by default in nav component
    Added "display:none" style to the user-menu div in the nav.blade.php file. This change hides the user-menu by default, improving the UI by showing the user-menu only when needed.

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -125,7 +125,8 @@
                 <div
                     x-show="userMenu"
                     class=" absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white dark:bg-gray-700 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                    role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+                    role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1"
+                    style="display: none;">
                     <a href="#"
                        x-on:click.prevent="slideOver = true, mobileMenu = false, settingsFlyout = false, userMenu = false"
                        class="block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600"
