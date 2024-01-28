3f304d241285f64bcef654071f35e0607b139de6
Holger Schmermbeck <hosch@mailbox.org>
2024-01-28 21:19:15 +0100

    remove comments

--- resources/views/livewire/shift.blade.php
+++ resources/views/livewire/shift.blade.php
@@ -4,8 +4,6 @@
             <x-dialog wire:model="show">
                 <x-dialog.open>
                     <button type="button"
-                            {{--                            wire:click="endShift"--}}
-                            {{--                            wire:confirm="{{ __('Do you want to end your shift and log out?') }}"--}}
                             class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline dark:text-white"
                             aria-label="Menu">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -32,18 +30,6 @@
                                    class="block text-center rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 dark:bg-gray-800 dark:text-white"
                                    placeholder="HH:MM">
                         </div>
-
-                        {{--                        <div>--}}
-                        {{--                            <label for="location-{{ $identifier }}"--}}
-                        {{--                                   class="block text-sm font-medium leading-6 text-gray-900 dark:text-white">{{ __('shift location') }}</label>--}}
-                        {{--                            <select wire:model="shift_location" id="location-{{ $identifier }}" name="location"--}}
-                        {{--                                    class="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-blue-600 sm:text-sm sm:leading-6 dark:bg-gray-800 dark:text-white">--}}
-                        {{--                                <option>{{ __('Select your location') }}</option>--}}
-                        {{--                                @foreach($locations as $location)--}}
-                        {{--                                    <option value="{{ $location->id }}">{{ $location->name }}</option>--}}
-                        {{--                                @endforeach--}}
-                        {{--                            </select>--}}
-                        {{--                        </div>--}}
 
                         <x-dialog.footer>
                             <x-dialog.close>
