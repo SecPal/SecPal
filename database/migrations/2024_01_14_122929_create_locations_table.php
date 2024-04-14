36af5dd173a504d4f76c28d5b90ab059c1989e39
Holger Schmermbeck <hosch@mailbox.org>
2024-04-14 12:54:46 +0200

    Make 'location' field nullable in locations table
    The code change updates the 'location' field in the locations table and makes it nullable. This ensures that it is not mandatory to provide a location every time when creating or updating entries in the table.

--- database/migrations/2024_01_14_122929_create_locations_table.php
+++ database/migrations/2024_01_14_122929_create_locations_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->foreignIdFor(Customer::class)->constrained();
             $table->string('name');
-            $table->string('location');
+            $table->string('location')->nullable();
             $table->timestamps();
         });
     }
