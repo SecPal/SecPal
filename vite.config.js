f24fd4b06f45128593c7e55022180b9803a69288
Holger Schmermbeck <hosch@mailbox.org>
2024-05-19 23:33:51 +0200

    Simplify vite.config.js by removing unnecessary codes
    The update cleans up vite.config.js file, by removing unnecessary imports and configurations. The removed code was mainly for setting server details like host, port and HTTPS, which is not necessary in this context.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,5 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-
-import fs from 'fs';
-
-const env = loadEnv('all', process.cwd());
 
 export default defineConfig({
     plugins: [
@@ -12,17 +8,4 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    server: {
-        host: true,
-        port: env.VITE_ASSET_PORT,
-        strictPort: true,
-        hmr: {
-            host: env.VITE_ASSET_HOST,
-            port: env.VITE_ASSET_PORT,
-        },
-        https: {
-            key: fs.readFileSync(env.VITE_PRIVKEY_PATH),
-            cert: fs.readFileSync(env.VITE_CERT_PATH),
-        },
-    },
 });
